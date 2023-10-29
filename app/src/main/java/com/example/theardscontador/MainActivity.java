bc90ad6bc1b4ab245f07ca5c45e63a5b16619c7a
Leonardo Rodriguez Vega <vegaleandro757@gmail.com>
2023-10-28 21:19:59 -0300

    Initial commit

---
+++ app/src/main/java/com/example/theardscontador/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.theardscontador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean iniciar = false;
+    private int counter = 0;
+    private TextView txtcontador;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtcontador = findViewById(R.id.txtcontador);
+    }
+
+    public void Start(View view){
+        if(!iniciar){
+            iniciar = true;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (iniciar){
+                        counter++;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                txtcontador.setText(String.valueOf(counter));
+                            }
+                        });
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+
+    public void Stop(View view){
+        iniciar=false;
+    }
+}
